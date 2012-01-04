5ee19a15eec4e39c9e84607c74ca6f60f1233bb1
francispapineau@gmail.com <francispapineau@gmail.com@5d37829d-8ef0-5259-2128-97a567d2b9cb>
2012-01-04 16:27:44 +0000

    added prototypes for functions for menus

--- Arduino Code/Arduino - TestVer4/Menus.c
+++ Arduino Code/Arduino - TestVer4/Menus.c
@@ -8,6 +8,54 @@
 
 
 void initMenusThroughMenus(void){
-    
+    // install the menus in memory
     
 }
+
+void toggleRightThroughVirtualMenus(void){
+
+    // move right in menu selection
+}
+
+void toggleLeftThroughVirtualMenus(void){
+
+    // move left in menu selection
+}
+
+void viewBatteryLevel(void){
+
+    // display only battery voltage
+
+}
+
+void viewStartupScreenConnectionType(void){
+
+   // display the different types of connections
+   // possible:
+   // -> USB
+   // -> XBee
+}
+
+void viewStartupScreenSplashScreen(void){
+
+   // display splash screen sprite
+}
+
+void viewSignalStrenght(void){
+
+  // display the signal strenght ICONS
+}
+
+void viewConnectedToIcon(void){
+  
+  // displays the connected to Icon on the top right
+  // hand side of the LCD display 
+}
+
+void viewBatteryIcon(void){
+
+  // shows the Battery ICON level on top right of
+  // the LCD
+}
+
+
